ec60eca83bb39d9f18dc9dfd4178bf92262fa4b7
PolluXxX <nathan.castelein@gmail.com>
2014-02-18 00:24:28 +0100

    Change output of list files

--- list.go
+++ list.go
@@ -6,7 +6,7 @@ import (
 )
 
 var cmdList = &Command{
-	UsageLine: "list",
+	UsageLine: "list [directory]",
 	Short:     "list your files on hubiC",
 	Long: `
 list gives you a complete list of all your files on hubiC
@@ -19,9 +19,29 @@ func init() {
 }
 
 func runList(cmd *Command, args []string) {
-	files, err := Account.List("/")
+    if len(args) > 1 {
+		help([]string{cmd.Name()})
+		return
+	}
+
+    path := ""
+    if len(args) > 0 {
+        path = args[0]
+    }
+
+	files, err := Account.List(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", files)
+
+    for _, file := range *files {
+        name := file.Name
+        if file.ContentType == "application/directory" {
+            name += "/"
+        }
+
+        if name != "" {
+            fmt.Println(name)
+        }
+    }
 }
